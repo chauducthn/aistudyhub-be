c5f839ae2f43feade0f7ddb2b0d61f8e30c3b8d1
chauducthn <chauducthien00@gmail.com>
2026-05-23 08:36:20 +0700

    init: setup backend services

---
+++ src/main/java/com/studyhub/aistudyhubbe/AistudyhubBeApplication.java
@@ -0,0 +1,13 @@
+package com.studyhub.aistudyhubbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AistudyhubBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AistudyhubBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyhub/aistudyhubbe/AistudyhubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhub.aistudyhubbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AistudyhubBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
